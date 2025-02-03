5c63dd8e03e83e6c5d9d8b3e79b17e3051f32027
rnovackwilson <reecenovackwilson@gmail.com>
2025-02-03 17:26:14 -0600

    Initial commit

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int myInt = 42;
+    float myFloat = 3.14;
+    double myDouble = 2.71828;
+    char myChar = 'C';
+    bool myBool = true;
+    string myString = "Hello, World!";
+
+    std:: cout << "Interger: " << myInt << std:: endl;
+    std:: cout << "Float: " << myFloat << std:: endl;
+    std:: cout << "Double: " << myDouble << std:: endl;
+    std:: cout << "Character: " << myChar << std:: endl;
+    std:: cout << "Boolean: " << myBool << std:: endl;
+    std:: cout << "String: " << myString << std:: endl;
+
+    
+    return 0;
+}
